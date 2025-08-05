617f8e63a62a496f135d0aca06e0d0e8a81b6710
APTTrialweb <geotechchronicle@gmail.com>
2025-08-05 22:23:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const secretCode = urlParams.get('code');
 if (!secretCode) {
   alert("Invalid QR Code URL!");
 } else {
-  fetch(`https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec?code=${secretCode}`)
+  fetch(`https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbw1r1xAIowX_6S4RNWtKBvr89dc_duG9C4OqavxCja1uux-0M9oZ09XFWyk_zfuw226wQ/exec/exec?code=${secretCode}`)
     .then(response => response.json())
     .then(data => {
       if (data.error) {
